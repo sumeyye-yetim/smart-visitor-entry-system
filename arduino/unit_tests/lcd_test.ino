3fa4d4e88386b2fdc3b97179c6e04f65921fa0cb
sumeyye-yetim <sumeyyeyetim10@gmail.com>
2026-05-03 18:28:52 +0200

    Create lcd_test.ino

---
+++ arduino/unit_tests/lcd_test.ino
@@ -0,0 +1,14 @@
+cpp
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Sistem Hazir");
+  lcd.setCursor(0, 1);
+  lcd.print("Kart Okutunuz");
+}
+void loop() {
+}
